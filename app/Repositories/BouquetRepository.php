6c97fdf894c5d7ae1fe99ccf0bed4c4f62649032
Alina Zahovora <alina.zahovora@nure.ua>
2023-06-07 22:47:00 +0300

    Decor, flower add, update

--- app/Repositories/BouquetRepository.php
+++ app/Repositories/BouquetRepository.php
@@ -17,4 +17,26 @@ class BouquetRepository extends BaseRepository
     {
         return BouquetValidator::class;
     }
+
+    public function create(array $attributes)
+    {
+        $configuration = $attributes['configuration'];
+        unset($attributes['configuration']);
+
+        $bouquet = parent::create($attributes);
+        $bouquet->flowers()->attach($attributes['flowers'], ["bouquet_flowers_amount" => 1]);
+        $bouquet->decors()->attach($attributes['decors'], ["bouquet_decors_amount" => 1]);
+
+        $bouquet->configuration = json_encode($configuration);
+        $bouquet->save();
+
+        $bouquet = $this->model->with(['flowers', 'decors'])->find($bouquet->id);
+
+        return $bouquet;
+    }
+
+    public function find($id, $columns = ['*'])
+    {
+        return $this->model->with(['flowers', 'decors'])->find($id);
+    }
 }
