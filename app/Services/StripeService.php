e8c1b6188269f590cffb4f249fbb452ec629a604
Artem Kuptsov <kuptsov210@gmail.com>
2023-06-14 00:08:27 +0300

    Add validation to flowers and decors amount

--- app/Services/StripeService.php
+++ app/Services/StripeService.php
@@ -3,10 +3,13 @@
 namespace App\Services;
 
 use App\Mail\OrderMail;
+use App\Models\Decor;
+use App\Models\Flower;
 use App\Models\Order;
 use App\Models\User;
 use App\Repositories\OrderRepository;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 
 class StripeService
@@ -18,19 +21,45 @@ class StripeService
         $totalMoney = $data["bouquet"]->total_price;
         $orderRepository = app(OrderRepository::class);
 
+        DB::beginTransaction();
+        try {
+            foreach ($data["bouquet"]->flowers as $flower) {
+                $existedFlower = Flower::find($flower->id);
+                if ($flower->pivot->bouquet_flowers_amount > $existedFlower->storage_flowers_amount) {
+                    throw new \Exception("Not enough flower $flower->id at system", 422);
+                }
+
+                $existedFlower->storage_flowers_amount -= $flower->pivot->bouquet_flowers_amount;
+                $existedFlower->save();
+            }
+
+            foreach ($data["bouquet"]->decors as $decor) {
+                $existedDecor = Decor::find($decor->id);
+                if ($decor->pivot->bouquet_decors_amount > $existedDecor->storage_decors_amount) {
+                    throw new \Exception("Not enough decor $decor->id at system", 422);
+                }
+
+                $existedDecor->storage_decors_amount -= $decor->pivot->bouquet_decors_amount;
+                $existedDecor->save();
+            }
+
+            $order = $orderRepository->create([
+                'user_id' => $user->id,
+                'amount' => $data["bouquet"]->amount,
+                'bouquet_id' => $data["bouquet"]->id,
+                'shop_id' => $data["shop_id"],
+            ]);
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+            throw $e;
+        }
+
         $user->charge(
             $totalMoney * 100, $paymentMethod
         );
 
-        $orderData = [
-            'user_id' => $user->id,
-            'amount' => $data["bouquet"]->amount,
-            'bouquet_id' => $data["bouquet"]->id,
-            'shop_id' => $data["shop_id"],
-        ];
-
-
-        $order = $orderRepository->create($orderData);
         $pdf = Pdf::loadView('pdfs.order_mail', ['order' => $order]);
         Mail::to(env("MAIL_TO"))->send(new OrderMail($order, $pdf->output()));
 
